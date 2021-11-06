34a82a87c0c1a57878852240182b0cf50f531f21
admin6750217 <58500194+admin6750217@users.noreply.github.com>
2021-11-06 11:36:36 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -17,12 +17,12 @@ sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generat
 #git clone https://github.com/linkease/istore.git package/istore
 #sirpdboy
 #git clone https://github.com/sirpdboy/sirpdboy-package.git package/sirpdboy-package
-git clone https://github.com/sirpdboy/luci-theme-opentopd.git package/luci-theme-opentopd
-git clone https://github.com/sirpdboy/luci-app-advanced.git package/luci-app-advanced
-git clone https://github.com/sirpdboy/netspeedtest.git package/netspeedtest
-git clone https://github.com/sirpdboy/luci-app-netdata.git package/luci-app-netdata
-git clone https://github.com/sirpdboy/luci-app-poweroffdevice.git package/luci-app-poweroffdevice
-git clone https://github.com/sirpdboy/luci-app-autotimeset.git package/luci-app-autotimeset
+#git clone https://github.com/sirpdboy/luci-theme-opentopd.git package/luci-theme-opentopd
+#git clone https://github.com/sirpdboy/luci-app-advanced.git package/luci-app-advanced
+#git clone https://github.com/sirpdboy/netspeedtest.git package/netspeedtest
+#git clone https://github.com/sirpdboy/luci-app-netdata.git package/luci-app-netdata
+#git clone https://github.com/sirpdboy/luci-app-poweroffdevice.git package/luci-app-poweroffdevice
+#git clone https://github.com/sirpdboy/luci-app-autotimeset.git package/luci-app-autotimeset
 
 
 #修正连接数（by ベ七秒鱼ベ）
